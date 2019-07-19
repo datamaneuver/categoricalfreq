be1b03569be03c11813bb6e3cec70fb611179f86
sandeepmarpini <51962825+sandeepmarpini@users.noreply.github.com>
2019-07-19 09:30:24 +0530

    Add files via upload

---
+++ R/categoricalfreq.R
@@ -0,0 +1,33 @@
+
+categoricalfreq <- function(data, target_var)
+{
+  d0 <- data[data[target_var] == 0, ]
+  d1 <- data[data[target_var] == 1, ]
+  categorical_columns = c()
+
+  for (i in 1:ncol(data))
+  {
+    if (is.factor(data[,i]))
+    {
+      values = c()
+      values = append(values, c(names(data[i])))
+      categorical_columns = append(categorical_columns, values, after = length(categorical_columns))
+    }
+  }
+
+  for (i in 1:length(categorical_columns))
+  {
+    j = data.frame()
+    k = data.frame()
+    l = data.frame()
+    cat('\nFrequency of Categories for varible',categorical_columns[i],'is sent to the excel')
+    j = as.data.frame(table(d0[categorical_columns[i]]))
+    k = as.data.frame(table(d1[categorical_columns[i]]))
+    m = k[,2]
+    l = cbind(j,m)
+    colnames(l) <- c('category','freq count for non events', 'freq count for events')
+    write.xlsx(l, file="categorical_variables.xlsx", sheetName=categorical_columns[i],append=TRUE, row.names=FALSE)
+  }
+
+}
+
